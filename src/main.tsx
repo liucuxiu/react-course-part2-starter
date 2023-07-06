1f58a5f3d4823058e19dc61e0ae3790db1560028
liucuxiu <xliucuxiux@gmail.com>
2023-07-06 14:04:17 +0700

    react query dev tool

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,23 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, // 5 minutes
+      staleTime: 10 * 1000, // 10 seconds
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +26,7 @@ ReactDOM.createRoot(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App/>
+      <ReactQueryDevtools/>
     </QueryClientProvider>
   </React.StrictMode>
 );
