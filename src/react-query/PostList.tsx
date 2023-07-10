53657cd6852f7e99d9b275eeba1868d358e62d79
liucuxiu <xliucuxiux@gmail.com>
2023-07-10 11:33:33 +0700

    Paginated Queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -3,8 +3,11 @@ import usePosts from './hooks/usePosts';
 
 
 const PostList = () => {
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+
   const [userId, setUserId] = useState<number>();
-  const { data: posts, isLoading, error } = usePosts(userId);
+  const { data: posts, isLoading, error } = usePosts({ page, pageSize });
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -27,6 +30,17 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        className="btn btn-primary mt-3"
+        onClick={() => setPage(page - 1)}>
+        Previous
+      </button>
+      <button
+        className="btn btn-primary mt-3 ms-1"
+        onClick={() => setPage(page + 1)}>
+        Next
+      </button>
     </>
 
   );
