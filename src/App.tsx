5b4c5a6fcd2e1f80799eadcbdced00c037b6c274
liucuxiu <xliucuxiux@gmail.com>
2023-07-10 11:02:48 +0700

    Parameterized queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-import TodoList from './react-query/TodoList';
 import PostList from './react-query/PostList';
 
 function App() {
   return (
     <>
-      <TodoList/>
       <PostList/>
     </>
 
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import usePosts from './hooks/usePosts';
 
 
 const PostList = () => {
-  const { data: posts, isLoading, error } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, isLoading, error } = usePosts(userId);
 
   if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select className="form-select mb-3"
+              onChange={(e) => setUserId(Number(e.target.value))}
+              value={userId}>
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
+
   );
 };
 
