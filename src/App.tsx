56d5b1dd9122dbc24c83e6260a264fd057d6ed53
liucuxiu <xliucuxiux@gmail.com>
2023-07-27 21:14:41 +0700

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import TodoForm from './react-query/TodoForm';
 import TodoList from './react-query/TodoList';
+import Counter from './state-management/Counter';
 
 function App() {
   return (
     <>
+      <Counter/>
       <TodoForm/>
       <TodoList/>
     </>
